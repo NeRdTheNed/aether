073c3a5dc8d6030fe6864da53b5f97c35119d9d6
Brandon Pearce <brandonpearce@gilded-games.com>
2014-10-30 17:12:09 +0800

    Fixed setup.bat

--- setup.bat
+++ setup.bat
@@ -1,15 +1,7 @@
 @echo off
-:: Variables
-set PATH=%PATH%;%DIRECTORY%
-
 title Aether II Development Workspace Setup
 
-:: Deleting OLD Eclipse Workspace
-IF EXIST eclipse (
-echo Deleting OLD eclipse workspace...
-del /q /s eclipse >nul
-)
-
 :: Call Gradle Setup
-call gradlew setupDecompWorkspace eclipse
+call gradlew setupDecompWorkspace
+call gradlew eclipse
 pause
